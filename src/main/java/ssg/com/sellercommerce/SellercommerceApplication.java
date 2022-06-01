0b80f067493e4320ccd582996add7bf2aa6c4643
kenokim <keno1413@gmail.com>
2022-06-01 18:12:50 +0900

    Chore : init

---
+++ src/main/java/ssg/com/sellercommerce/SellercommerceApplication.java
@@ -0,0 +1,13 @@
+package ssg.com.sellercommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SellercommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SellercommerceApplication.class, args);
+	}
+
+}
