edfbc3064785a423b34dd635871ee75504097566
kenokim <keno1413@gmail.com>
2022-06-02 18:38:22 +0900

    Feat : 업체생성 테스트 케이스 작성

---
+++ src/test/java/ssg/com/sellercommerce/service/CompanyServiceTest.java
@@ -0,0 +1,31 @@
+package ssg.com.sellercommerce.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+@Transactional
+class CompanyServiceTest {
+    @MockBean private ItemService itemService;
+
+    @Autowired private CompanyService companyService;
+
+    private final String companyName = "이상해씨샵";
+    private final Long businessNumber = 1000000000L;
+    private final Long phoneNumber = 1012345678L;
+    private final String address = "hello";
+
+    @Test
+    public void CreateCompany_Success() {
+        when(itemService.isValidCompanyName(companyName))
+                .thenReturn(true);
+        assertThat(companyService.register(companyName, businessNumber, phoneNumber, address)).isGreaterThan(1000000000L).isNotNull();
+    }
+}
